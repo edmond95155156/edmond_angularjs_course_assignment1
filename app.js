8d9cd5e362739ec4b34ab3bc4e423eddf7afb284
edmond95155156 <51832514+edmond95155156@users.noreply.github.com>
2021-06-10 02:09:29 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+(
+    function(){
+        angular.module("MyApp",[]).controller("MyController", function($scope){
+            $scope.message="";
+            $scope.result="";
+            $scope.checkExcessiveFood=function (){
+                $scope.items=$scope.message.split(',');
+                $scope.number=$scope.items.length;
+                if ($scope.message==""){
+                    $scope.result="Empty!";
+                }
+                else if($scope.number>0 && $scope.number<=3){
+                    $scope.result="Enjoy!";
+                }else if($scope.number>3){
+                    $scope.result="Too Much!";
+                }
+
+
+               
+            }
+        });
+    }
+)();
