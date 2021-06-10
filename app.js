a0b5b32ece4df3ab5c6a3c04be28d99e834ff448
edmond95155156 <51832514+edmond95155156@users.noreply.github.com>
2021-06-10 20:36:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
                 $scope.items=$scope.message.split(',');
                 $scope.number=$scope.items.length;
                 if ($scope.message==""){
-                    $scope.result="Empty!";
+                    $scope.result="Please enter data first";
                 }
                 else if($scope.number>0 && $scope.number<=3){
                     $scope.result="Enjoy!";
